4ee4d7182e9b329491a21f855ba5edd2ff57dcf4
rahatsummer <rahatsummer@gmail.com>
2023-12-16 17:46:12 +0600

    store and carts functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
